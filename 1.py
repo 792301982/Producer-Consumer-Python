c1e8c303a952c8b89f93d53483e58faeabe831ab
Yangning <792301982@qq.com>
2019-11-21 13:29:38 +0800

    python版本生产者消费者

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title('生产者消费者')
+root.title('生产者消费者 By Yangning')
 root.geometry('400x400')
 
 Label(root, text='缓冲区：', bg='red',
@@ -21,8 +21,7 @@ Label(root, textvariable=zuse_text_list[1], bg='green', width=10, height=2).grid
     row=2, column=3)
 Label(root, textvariable=zuse_text_list[2], bg='green', width=10, height=2).grid(
     row=2, column=4)
-Label(root, textvariable=zuse_text_list[3], bg='green', width=10, height=2).grid(
-    row=2, column=5)
+
 
 Label(root, text='等待队列：', bg='red',
       width=10, height=2).grid(row=3, column=1)
@@ -33,8 +32,7 @@ Label(root, textvariable=wait_text_list[1], bg='green', width=10, height=2).grid
     row=3, column=3)
 Label(root, textvariable=wait_text_list[2], bg='green', width=10, height=2).grid(
     row=3, column=4)
-Label(root, textvariable=wait_text_list[3], bg='green', width=10, height=2).grid(
-    row=3, column=5)
+
 
 huanchong_list = list()
 zuse_list = list()
@@ -53,28 +51,52 @@ def charu(x, l):
 
 
 def check_zuse():
-    if '生产者1' in zuse_list:
+    if '生产者1' in zuse_list or len(zuse_list)>=3:
         Button(root, text='生产者1', width=10, height=1,
                state=DISABLED).grid(row=4, column=1)
-    if '生产者2' in zuse_list:
+    if '生产者2' in zuse_list or len(zuse_list)>=3:
         Button(root, text='生产者2', width=10, height=1,
                state=DISABLED).grid(row=4, column=2)
-    if '生产者3' in zuse_list:
+    if '生产者3' in zuse_list or len(zuse_list)>=3:
         Button(root, text='生产者3', width=10, height=1,
                state=DISABLED).grid(row=4, column=3)
+    if '生产者4' in zuse_list or len(zuse_list)>=3:
+        Button(root, text='生产者4', width=10, height=1,
+               state=DISABLED).grid(row=4, column=4)
 
 
 def check_wait():
-    if '消费者1' in wait_list:
+    if '消费者1' in wait_list or len(wait_list)>=3:
         Button(root, text='消费者1', width=10, height=1,
                state=DISABLED).grid(row=5, column=1)
-    if '消费者2' in wait_list:
+    if '消费者2' in wait_list or len(wait_list)>=3:
         Button(root, text='消费者2', width=10, height=1,
                state=DISABLED).grid(row=5, column=2)
-    if '消费者3' in wait_list:
+    if '消费者3' in wait_list or len(wait_list)>=3:
         Button(root, text='消费者3', width=10, height=1,
                state=DISABLED).grid(row=5, column=3)
+    if '消费者4' in wait_list or len(wait_list)>=3:
+        Button(root, text='消费者4', width=10, height=1,
+               state=DISABLED).grid(row=5, column=4)
 
+def refresh_button():
+    #刷新按钮
+    Button(root, text='生产者1', width=10, height=1,
+           command=lambda: shengchan(1)).grid(row=4, column=1)
+    Button(root, text='生产者2', width=10, height=1,
+           command=lambda: shengchan(2)).grid(row=4, column=2)
+    Button(root, text='生产者3', width=10, height=1,
+           command=lambda: shengchan(3)).grid(row=4, column=3)
+    Button(root, text='生产者4', width=10, height=1,
+           command=lambda: shengchan(4)).grid(row=4, column=4)
+    Button(root, text='消费者1', width=10, height=1,
+           command=lambda: xiaofei(1)).grid(row=5, column=1)
+    Button(root, text='消费者2', width=10, height=1,
+           command=lambda: xiaofei(2)).grid(row=5, column=2)
+    Button(root, text='消费者3', width=10, height=1,
+           command=lambda: xiaofei(3)).grid(row=5, column=3)
+    Button(root, text='消费者4', width=10, height=1,
+           command=lambda: xiaofei(4)).grid(row=5, column=4)
 
 def shengchan(number):
     global huanchong_list, wait_list, zuse_list
@@ -93,18 +115,7 @@ def shengchan(number):
     charu(huanchong_text_list, huanchong_list)
     charu(wait_text_list, wait_list)
     charu(zuse_text_list, zuse_list)
-    Button(root, text='生产者1', width=10, height=1,
-           command=lambda: shengchan(1)).grid(row=4, column=1)
-    Button(root, text='生产者2', width=10, height=1,
-           command=lambda: shengchan(2)).grid(row=4, column=2)
-    Button(root, text='生产者3', width=10, height=1,
-           command=lambda: shengchan(3)).grid(row=4, column=3)
-    Button(root, text='消费者1', width=10, height=1,
-           command=lambda: xiaofei(1)).grid(row=5, column=1)
-    Button(root, text='消费者2', width=10, height=1,
-           command=lambda: xiaofei(2)).grid(row=5, column=2)
-    Button(root, text='消费者3', width=10, height=1,
-           command=lambda: xiaofei(3)).grid(row=5, column=3)
+    refresh_button()
     check_zuse()
     check_wait()
 
@@ -115,6 +126,8 @@ Button(root, text='生产者2', width=10, height=1,
        command=lambda: shengchan(2)).grid(row=4, column=2)
 Button(root, text='生产者3', width=10, height=1,
        command=lambda: shengchan(3)).grid(row=4, column=3)
+Button(root, text='生产者4', width=10, height=1,
+       command=lambda: shengchan(4)).grid(row=4, column=4)
 
 
 def xiaofei(number):
@@ -133,27 +146,19 @@ def xiaofei(number):
     charu(huanchong_text_list, huanchong_list)
     charu(wait_text_list, wait_list)
     charu(zuse_text_list, zuse_list)
-    Button(root, text='生产者1', width=10, height=1,
-           command=lambda: shengchan(1)).grid(row=4, column=1)
-    Button(root, text='生产者2', width=10, height=1,
-           command=lambda: shengchan(2)).grid(row=4, column=2)
-    Button(root, text='生产者3', width=10, height=1,
-           command=lambda: shengchan(3)).grid(row=4, column=3)
-    Button(root, text='消费者1', width=10, height=1,
-           command=lambda: xiaofei(1)).grid(row=5, column=1)
-    Button(root, text='消费者2', width=10, height=1,
-           command=lambda: xiaofei(2)).grid(row=5, column=2)
-    Button(root, text='消费者3', width=10, height=1,
-           command=lambda: xiaofei(3)).grid(row=5, column=3)
+    refresh_button()
     check_zuse()
     check_wait()
 
 
+#布置消费者按钮
 Button(root, text='消费者1', width=10, height=1,
        command=lambda: xiaofei(1)).grid(row=5, column=1)
 Button(root, text='消费者2', width=10, height=1,
        command=lambda: xiaofei(2)).grid(row=5, column=2)
 Button(root, text='消费者3', width=10, height=1,
        command=lambda: xiaofei(3)).grid(row=5, column=3)
+Button(root, text='消费者4', width=10, height=1,
+       command=lambda: xiaofei(4)).grid(row=5, column=4)
 
 mainloop()
